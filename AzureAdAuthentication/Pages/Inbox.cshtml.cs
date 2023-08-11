78dfd82888bbcd0365c04d1008fc65fb1e8095e0
Nahuel Gonzalez <nahuel.gonzalez@coolblue.nl>
2023-08-11 23:05:42 +0200

    Ejemplo AzureAd Auth funcionando

---
+++ AzureAdAuthentication/Pages/Inbox.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AzureAdAuthentication.Pages
+{
+    public class InboxModel : PageModel
+    {
+        private readonly ILogger<InboxModel> _logger;
+
+        public InboxModel(ILogger<InboxModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
